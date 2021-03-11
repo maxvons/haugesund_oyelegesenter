1b81ba1de72de4ef25c4bb0b08fbebbfdbc7e231
Maximilian von Stephanides <maximilianvons@gmail.com>
2021-03-11 23:29:15 +0100

    Optimize JS and remove unused font

--- navigation.js
+++ navigation.js
@@ -43,3 +43,20 @@ function toggleMobileMenuNavLink() {
         continueIcon.classList.remove("continue-icon-responsive");
     }
 }
+
+window.onload = function() {
+    if (
+        "IntersectionObserver" in window &&
+        "IntersectionObserverEntry" in window &&
+        "intersectionRatio" in window.IntersectionObserverEntry.prototype
+        ) {
+        let observer = new IntersectionObserver(entries => {
+            if (entries[0].boundingClientRect.y < 0) {
+            document.body.classList.add("header-not-at-top");
+            } else {
+            document.body.classList.remove("header-not-at-top");
+            }
+        });
+        observer.observe(document.querySelector("#top-of-site-pixel-anchor"));
+    }
+}
